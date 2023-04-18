4869b7ba6c1181582268ccbf8b239c5043a2a486
olehhapuk <oleggapuk20@gmail.com>
2023-04-18 19:12:08 +0300

    added initial notifications load

--- src/components/Navbar/Notifications/Notifications.js
+++ src/components/Navbar/Notifications/Notifications.js
@@ -3,10 +3,14 @@ import {
   MdOutlineNotificationsActive,
 } from 'react-icons/md';
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import { socket } from 'config/socket';
 import styles from './Notifications.module.css';
 import { useClickOutside } from 'hooks/useClickOutside';
 import NotificationItem from './NotificationItem';
+import * as usersService from 'services/users.service';
+import * as authSelectors from 'redux/auth/auth.selectors';
 
 const avatarUrl =
   'https://res.cloudinary.com/bevzyksoi/image/upload/v1668016321/sbcf-default-avatar_dykn6i.png';
@@ -17,6 +21,8 @@ function Notifications() {
   const [visible, setVisible] = useState(false);
   const [notificationItemsList, setNotificationItemsList] = useState([]);
 
+  const user = useSelector(authSelectors.getUser);
+
   useClickOutside(`.${styles.container}`, () => {
     setVisible(false);
   });
@@ -25,7 +31,7 @@ function Notifications() {
     setVisible((prev) => !prev);
   }
   function onNewNotification(notification) {
-    setNotificationItemsList((prev) => [...prev, notification]);
+    setNotificationItemsList((prev) => [notification, ...prev]);
   }
 
   useEffect(() => {
@@ -35,6 +41,16 @@ function Notifications() {
     };
   }, []);
 
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    usersService
+      .getUserNotifications(user._id)
+      .then((data) => setNotificationItemsList(data.reverse()));
+  }, [user]);
+
   return (
     <div className={styles.container}>
       <button type="button" className={styles.toggler} onClick={toggle}>
@@ -43,17 +59,21 @@ function Notifications() {
 
       {visible && (
         <div className={styles.notifications}>
-          {notificationItemsList.map((item) => {
-            return (
-              <NotificationItem
-                key={item._id}
-                id={item._id}
-                type={item.type}
-                user={item.user}
-                target={item.target}
-              />
-            );
-          })}
+          {notificationItemsList.length > 0 ? (
+            notificationItemsList.map((item) => {
+              return (
+                <NotificationItem
+                  key={item._id}
+                  id={item._id}
+                  type={item.type}
+                  user={item.user}
+                  target={item.target}
+                />
+              );
+            })
+          ) : (
+            <p>No notifications</p>
+          )}
         </div>
       )}
     </div>
